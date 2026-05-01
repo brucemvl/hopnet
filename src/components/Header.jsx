6829c2b1649891f8de0e7601e3f85aaadb4c7f99
bruce <bruce.monnerville@hotmail.fr>
2026-05-01 21:59:22 +0200

    init projet

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import logo from "../assets/logo.jpg"
+import "../styles/Header.scss"
+
+function Header(){
+
+return <header>
+    <img src={logo} />
+</header>
+}
+
+export default Header
---
+++ src/pages/Index.jsx
@@ -0,0 +1,139 @@
+import "../styles/Index.scss"
+import prestations1 from "../assets/prestations1.png"
+import prestations2 from "../assets/prestations222.png"
+import prestations3 from "../assets/prestations3.png"
+import prestations4 from "../assets/prestations4.png"
+import prestations5 from "../assets/prestations5.png"
+import prestations6 from "../assets/prestations6.png"
+import { useEffect } from "react";
+
+function Index(){
+
+  useEffect(() => {
+    const items = document.querySelectorAll(".fade-item");
+
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add("visible");
+          } else {
+            entry.target.classList.remove("visible"); // 🔥 important
+          }
+        });
+      },
+      { threshold: 0.2 }
+    );
+
+    items.forEach(item => observer.observe(item));
+
+    return () => observer.disconnect();
+  }, []);
+
+return (
+<div className="blocpage">
+    <section>
+   <h2>Qui sommes nous?</h2>
+
+<p>
+    Spécialisée dans l’entretien d’immeubles, résidences et copropriétés, nous proposons un service de nettoyage exigeant, pensé pour valoriser durablement vos espaces.
+
+Nous accompagnons propriétaires, gestionnaires et syndics avec une approche simple : offrir un cadre propre, soigné et agréable à vivre, sans compromis sur la qualité
+</p>
+</section>
+
+<section className="contexte">
+
+<h2>Contexte & enjeux</h2>
+
+<h3>Les parties communes sont le reflet immédiat d’une résidence</h3>
+
+<article>
+<h4>Au quotidien, leur entretien conditionne</h4>
+
+<ul>
+<li>Le confort des occupants</li>
+<li>La perception des lieux</li>
+<li>La valorisation du bien</li>
+</ul>
+<p>
+Un nettoyage maîtrisé permet de maintenir des espaces propres, soignés et agréables, tout en préservant durablement les installations
+</p>
+</article>
+
+<article>
+<h4>Aujourd’hui, les attentes sont claires</h4>
+
+<ul>
+<li>Une qualité constante</li>
+<li>Une présence fiable</li>
+<li>Une gestion sans contrainte</li>
+</ul>
+<p>C’est cette exigence que nous intégrons dans chacune de nos interventions</p>
+</article>
+</section>
+
+<section className="prestations">
+<h2>Nos prestations</h2>
+<h3>Des interventions sur mesure, adaptées à chaque résidence</h3>
+
+<ul>
+<li className="fade-item">
+    <img src={prestations1} alt="image hall"/>
+    <h4>Entretien des halls et espaces d’accueil</h4>
+    </li>
+
+<li className="fade-item">
+        <img src={prestations2} alt="image nettoyage escaliers et paliers"/>
+<h4>Nettoyage des escaliers, paliers et circulations</h4>
+</li>
+
+<li className="fade-item">
+            <img src={prestations3} alt="image entretien ascenceur"/>
+<h4>Entretien des ascenseurs</h4>
+</li>
+
+<li className="fade-item">
+                <img src={prestations4} alt="image nettoyage parking"/>
+<h4>Nettoyage des parkings</h4>
+</li>
+
+<li className="fade-item">
+                <img src={prestations5} alt="image entretien local poubelles"/>
+<h4>Gestion et entretien des espaces déchets</h4>
+</li>
+
+<li className="fade-item">
+                    <img src={prestations6} alt="image nettoyage vitre"/>
+    <h4>Nettoyage de la vitrerie</h4>
+    </li>
+</ul>
+
+</section>
+
+<section className="exigence">
+
+<h2>Notre exigence</h2>
+
+
+<article>
+
+<ul>
+<li>Interventions régulières et maîtrisées</li>
+<li>Équipes formées, discrètes et professionnelles</li>
+<li>Suivi qualité constant</li>
+<li>Réactivité et adaptabilité</li>
+</ul>
+<p>
+Chaque détail compte. C’est cette exigence qui fait la différence au quotidien</p>
+</article>
+
+
+</section>
+
+</div>
+)
+
+}
+
+export default Index
